2ec97807620729b93fe04c3c71ef06f2ef3109e3
Nicolas-Barboza <nicobarboza85@email.com>
2026-04-14 01:55:38 -0300

    Primer commit

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+// Script.js - MÁXIMO 5 LÍNEAS - Solo para dark mode
+document.getElementById('darkmode-toggle').addEventListener('click', () => {
+  document.documentElement.classList.toggle('dark-mode');
+});
